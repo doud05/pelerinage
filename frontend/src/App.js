5ce49138cac2ca153a3def951da566e62153863b
doud05 <eleconte3@aol.com>
2024-12-05 16:57:23 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,16 @@
-import React, { useState, useEffect } from 'react';
+// src/App.js
+import React from 'react';
+import ClientList from './components/ClientList';
 
 function App() {
-    // Déclaration des états pour les clients, le chargement, et les erreurs
-    const [clients, setClients] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    // Utilisation de useEffect pour faire l'appel API lors du montage du composant
-    useEffect(() => {
-        console.log('Tentative de récupération des clients...');
-        
-        // Utilisation de fetch ou axios pour faire la requête GET
-        fetch('http://localhost:5000/api/clients')
-            .then((response) => {
-                console.log('Réponse reçue:', response);
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des clients');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                console.log('Données reçues:', data);
-                setClients(data); // Mise à jour de l'état clients avec les données reçues
-                setLoading(false); // Arrêt du chargement
-            })
-            .catch((error) => {
-                console.error('Erreur :', error);
-                setError('Erreur lors de la récupération des clients');
-                setLoading(false);
-            });
-    }, []);
-
-    // Affichage pendant le chargement
-    if (loading) {
-        return <div>Chargement des données...</div>;
-    }
-
-    // Affichage en cas d'erreur
-    if (error) {
-        return <div>{error}</div>;
-    }
-
-    // Affichage des données des clients lorsque tout est prêt
     return (
         <div className="App">
-            <h1>Liste des Clients</h1>
-            <ul>
-                {clients.map((client) => (
-                    <li key={client.id}>
-                        {client.titre} {client.nom} {client.prenom}
-                    </li>
-                ))}
-            </ul>
+            <header className="App-header">
+                <h1>Bienvenue sur notre Application de Pèlerinages</h1>
+            </header>
+            <ClientList /> {/* Ajout du composant ClientList ici */}
         </div>
     );
 }
 
 export default App;
-
