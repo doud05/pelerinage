32994b3a66bf1a38bb5bb6593f14f6c4c4d200b9
doud05 <eleconte3@aol.com>
2024-12-05 15:37:58 +0100

    Update db.js
    ajout de log

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = new Pool({
     user: process.env.DB_USER, // Par exemple : 'doud'
     host: process.env.DB_HOST, // Par exemple : 'le455538-001.eu.clouddb.ovh.net'
